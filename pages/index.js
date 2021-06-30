057ce643efad81b6a1c6c6218c6d5cd038939ed9
Nicholas Saravia <nicholas.saravia@worldacceptance.com>
2021-06-30 09:57:51 -0400

    base url text

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      {process.env.NEXT_PUBLIC_BASE_URL}
+      Base Url: {process.env.NEXT_PUBLIC_BASE_URL}
     </div>
   )
 }
